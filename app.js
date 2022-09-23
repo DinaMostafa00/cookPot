913d740f6a431061eae63ea9304e2f5384fc7b42
DinaMo96 <93136184+DinaMo96@users.noreply.github.com>
2022-09-23 16:11:42 +0200

    dbs

--- app.js
+++ app.js
@@ -2,6 +2,22 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const data = require("./data.js");
+
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("recipesDatabase.db");
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS recipes (id INTEGER PRIMARY KEY, title TEXT, description TEXT, ingredients TEXT, directions TEXT)"
+);
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS blogPosts (id INTEGER PRIMARY KEY, title TEXT, content TEXT, source TEXT)"
+);
+
+db.run(
+  "CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY, commenterName TEXT, title TEXT, comment TEXT, blogPostId INTEGER, FOREIGN KEY (blogPostId) REFERENCES blogPosts(id) )"
+);
+
 const app = express();
 
 ////engine
@@ -27,17 +43,26 @@ app.get("/", function (request, response) {
   response.render("start.hbs");
 });
 
+///recipes
 app.get("/recipes", function (request, response) {
-  const model = { recipes: data.recipes };
-
-  response.render("recipes.hbs", model);
+  const query = "SELECT * FROM recipes";
+  db.all(query, function (error, recipes) {
+    const model = { recipes };
+    response.render("recipes.hbs", model);
+  });
 });
+
+////////
 
 app.get("/blogs", function (request, response) {
-  const model = { blogs: data.blogs };
+  const query = "SELECT * FROM blogPosts";
+  db.all(query, function (error, blogPosts) {
+    const model = { blogPosts };
 
-  response.render("blogs.hbs", model);
+    response.render("blogs.hbs", model);
+  });
 });
+////////
 
 app.get("/contactme", function (request, response) {
   response.render("contactMe.hbs");
@@ -66,19 +91,32 @@ app.post("/blogs/:id", function (request, response) {
   response.redirect("/blogs/" + id);
 });
 
-////get
+////get IDs
+
 app.get("/recipes/:id", function (request, response) {
   const id = request.params.id; //to get the actual value of the id
-  const recipe = data.recipes.find((recipe) => recipe.id == id); // we call a method on this array (find) to find a project whose id is equal to what stored in the id variable
-  const model = { recipe: recipe };
-  response.render("singleRecipe.hbs", model);
+  const query = "SELECT * FROM recipes WHERE id= ?";
+  const values = [id];
+  db.get(query, values, function (error, recipe) {
+    const model = { recipe };
+    response.render("singleRecipe.hbs", model);
+  });
+
+  // const recipe = data.recipes.find((recipe) => recipe.id == id); // we call a method on this array (find) to find a project whose id is equal to what stored in the id variable
 });
 
+////////
 app.get("/blogs/:id", function (request, response) {
   const id = request.params.id;
-  const blog = data.blogs.find((blog) => blog.id == id);
-  const model = { blog: blog, comments: data.comments };
-  response.render("singleBlog.hbs", model);
+  const query = "SELECT * FROM blogPosts WHERE id= ?";
+  const values = [id];
+  db.get(query, values, function (error, blogPosts) {
+    const model = { blog };
+    response.render("singleBlog.hbs", model);
+  });
+
+  // const model = { blog: blog, comments: data.comments };
+  // const blog = data.blogs.find((blog) => blog.id == id);
 });
 
 app.listen(8080);
